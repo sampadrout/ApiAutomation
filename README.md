b7e6d9e3a1b6cca2b3c3cfab7121bb9c54ec0027
SrinuK <srinuk@thoughtworks.com>
2018-09-23 13:01:31 +0530

    Pusing the code repo for API Test Automation

---
+++ README.md
@@ -0,0 +1,11 @@
+API Test Automation using supertest(javascript) framework
+
+Steps to run:
+Pre-req: One should have node & npm installed on their machine before running the tests
+
+1. First of all clone this repo
+2. Open the repo in any IDE
+3. Run "npm install or npm i"
+4. Now run "npm test"
+
+supertest gotta fluent API and very easy to use the APIs using javascript
---
+++ api_testing.spec.js
@@ -0,0 +1,43 @@
+let request = require('supertest');
+let payload = require('./payload.json');
+let API = 'http://jsonplaceholder.typicode.com';
+
+describe('API Testing', function () {
+
+   test('GET method testing', function () {
+       request('http://jsonplaceholder.typicode.com').get('/users/1')
+           .set('Accept', 'application/json')
+           .set('Content-Type', 'application/json')
+           .end(function (err, res) {
+               expect(res.body.name).toBe('Leanne Graham');
+           })
+   });
+
+    test('DELETE method testing', function () {
+        request('http://jsonplaceholder.typicode.com').delete('/users/3')
+            .set('Accept', 'application/json')
+            .end(function (err, res) {
+                expect(res.statusCode).toBe(200);
+            })
+    });
+
+    test('POST method testing', function () {
+        request(API).post('/users')
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .send(payload.createUser)
+            .end(function (err, res) {
+                expect(res.statusCode).toBe(201);
+            })
+    });
+
+    test('PUT method testing', function () {
+        request(API).put('/users/2')
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .send(payload.updateUser)
+            .end(function (err, res) {
+                expect(res.statusCode).toBe(200);
+            })
+    })
+});
